c31038f4172242a8221443303a7c99eb9d459929
Greg <sharpdressedcodes@users.noreply.github.com>
2017-04-28 13:31:54 +1000

    bxms-323 change site.name (#50)

--- src/app-elle-site/config/site.js
+++ src/app-elle-site/config/site.js
@@ -2,7 +2,7 @@ export default {
     dateTimeFormat: 'MMM DD, YYYY h:mma',
     host: 'http://dev.elle-site.bauer-media.net.au',
     prodDomain: 'www.elle.com.au',
-    name: 'Elle',
+    name: 'ELLE',
     shortName: 'Elle',
     prefix: 'ELLE',
     network: 'wn'
