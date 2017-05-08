1b2a4862bd216d4e5deda9392286eb853ebc9148
Greg <sharpdressedcodes@users.noreply.github.com>
2017-05-08 14:27:39 +1000

    fix alignment issues for social logo (#58)

--- src/app-hb-site/config/site.js
+++ src/app-hb-site/config/site.js
@@ -2,7 +2,7 @@ export default {
     dateTimeFormat: 'MMM DD, YYYY h:mma',
     host: 'http://dev.hb-site.bauer-media.net.au',
     prodDomain: 'www.harpersbazaar.com.au',
-    name: "Harper's Bazaar",
+    name: "Harper's BAZAAR",
     shortName: "Harper's",
     prefix: 'HB',
     network: 'wn'
