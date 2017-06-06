d158008655cb1ea723b0de87c548bbf426b68064
Dean Burge <db@users.noreply.github.com>
2017-06-06 15:06:09 +1000

    add protection for recursive express response object (#98)

--- src/fluxibleLoggerPlugin.js
+++ src/fluxibleLoggerPlugin.js
@@ -7,9 +7,11 @@ export default function loggerPlugin() {
             return {
                 plugActionContext(actionContext) {
                     const originalActionDispatch = actionContext.dispatch;
-                    actionContext.dispatch = function(type, payload) {
-                        if (type.includes('_FAILED')) {
-                            logger.error(`${type} ${this.rootId}`, { error: payload.response.error });
+                    actionContext.dispatch = function dispatch(type, payload) {
+                        if (type.includes('_FAILED') || type === 'PAGE_NOT_FOUND') {
+                            let err = payload;
+                            if (payload.response && payload.response.error) err = payload.response.error;
+                            logger.error(`${type} ${this.rootId}`, err);
                         } else if (process.env.APP_DEBUG === 'true') {
                             logger.debug(`${type} ${this.rootId}`);
                         }
