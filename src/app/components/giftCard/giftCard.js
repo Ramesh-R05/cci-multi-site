30183a074192188bbbe5933ac74f51252fbc843c
Greg <sharpdressedcodes@users.noreply.github.com>
2018-02-13 15:35:45 +1100

    Replace gift card svg with img (#320)

--- src/app/components/giftCard/giftCard.js
+++ src/app/components/giftCard/giftCard.js
@@ -1,6 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import InlineSVG from 'react-inlinesvg';
-import { canUseDOM } from 'exenv';
 import get from 'lodash/object/get';
 
 export default class GiftCard extends Component {
@@ -14,23 +12,18 @@ export default class GiftCard extends Component {
     static DEFAULT_URL = 'gift-card-large';
     static DEFAULT_BUTTON_TEXT = 'Buy Now';
 
-    static contextTypes = {
-        config: PropTypes.object
-    };
-
     shouldComponentUpdate = () => false;
 
     render() {
         const { brand } = this.props;
         const url = get(brand, 'giftCard.url', GiftCard.DEFAULT_URL);
-        const imageUrl = 'assets/images/gift-card.svg';
         const buttonText = get(brand, 'giftCard.buttonText', GiftCard.DEFAULT_BUTTON_TEXT);
-        const img = <img alt="" src={imageUrl} />;
+        const imageUrl = 'assets/images/gift-card.png';
 
         return (
             <div className="gift-card-container column medium-6 large-12">
                 <div className="gift-card-image">
-                    { canUseDOM ? <InlineSVG src={imageUrl}>{ img }</InlineSVG> : img }
+                    <img alt="Gift Card" src={imageUrl} />
                 </div>
                 <div className="gift-card-button">
                     <a className={`gtm-gift-card-${brand.id}`} target="_blank" href={url}>
