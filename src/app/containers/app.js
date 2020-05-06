c7d035674c2c7833ff877800c99e95828f6dfea5
Kieran Jones <jones.kieran@hotmail.com>
2020-05-06 16:26:14 +1000

    [BXMA-2518] Fix no-js styling (#816)
    * Added no-js css directly to page rather than use class that wasn't working
    
    * Code styling

--- src/app/containers/app.js
+++ src/app/containers/app.js
@@ -4,7 +4,6 @@ import { connectToStores, provideContext } from '@bxm/flux';
 import AdManager from '@bxm/ad/lib/google/components/adManager';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import PropTypes from 'prop-types';
-import canUseDOM from 'exenv';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { handleHistory } from 'fluxible-router';
 import ErrorPage from '../components/page/error';
@@ -86,13 +85,32 @@ class Application extends Component {
             fontFamily: '"Montserrat-Regular", sans-serif'
         });
 
-        const className = canUseDOM ? '' : 'no-js';
-
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
-                <div className={className}>
-                    <Handler currentUrl={currentRoute && currentRoute.url} nodeType={nodeType} theme={theme} siteAlert={siteAlert} />
-                </div>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<style type="text/css">
+                            .load-more {
+                                display: none !important;
+                            }
+                            .pagination {
+                                padding-left: 15px;
+                                padding-right: 15px;
+                                width: 100%;
+                                float: left;
+                                display: block !important;
+                                text-align: center;
+                            }
+                            .pagination .button {
+                                margin-left: 10px;
+                            }
+                            .lazyload {
+                                display: none !important;
+                            }
+                        </style>`
+                    }}
+                />
+                <Handler currentUrl={currentRoute && currentRoute.url} nodeType={nodeType} theme={theme} siteAlert={siteAlert} />
             </MuiThemeProvider>
         );
     }
